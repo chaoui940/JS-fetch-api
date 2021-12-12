0ce89dfa1926ec08769ccb92d75feca84ea85dee
chaoui940 <daging129@gmail.com>
2021-12-12 14:58:26 +0100

    *

--- script.js
+++ script.js
@@ -15,3 +15,12 @@ fetch('https://jsonplaceholder.typicode.com/posts/1')
 	.catch(err=> {
 		console.log('Failed')
 	})
+
+	fetch('https://jsonplaceholder.typicode.com/posts/1')
+	.then(res=> {
+		console.log('res.ok, res status')
+	})
+
+	.catch(err=> {
+		console.log('Failed')
+	})
